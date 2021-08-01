9e17281b295612d5c37c2cb76c7a1dd427abf991
Xingxin HE <xingxin.he@mail.polimi.it>
2021-08-02 01:48:15 +0800

    libigl_102

--- Library/libigl/101_FileIO/main.cpp
+++ Library/libigl/101_FileIO/main.cpp
@@ -1,14 +1,22 @@
 #include <igl/readOFF.h>
 #include <igl/writeOBJ.h>
 #include <iostream>
+#include <string>
 
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
 
 int main(int argc, char* argv[])
 {
+	std::string path = "/path/to/directory";
+	std::cout << "Welcome to libigl 101!" << std::endl;
+	std::cout << "Please type anything and press enter to continue...\n" << std::endl;
+	std::string input = "";
+	std::cin >> input;
 	// Load a mesh in OFF format
 	igl::readOFF("../data/cube.off", V, F);
+
+	std::cout << "Mesh was read succeeded." << std::endl;
 
 	// Print the vertices and faces matrices
 	std::cout << "Vertices: " << std::endl << V << std::endl;
@@ -16,4 +24,6 @@ int main(int argc, char* argv[])
 
 	// Save the mesh in OBJ format
 	igl::writeOBJ("101_FileIO_cube.obj", V, F);
+	std::cout << "\nPlease type anything and press enter to continue..." << std::endl;
+	std::cin >> input;
 }
