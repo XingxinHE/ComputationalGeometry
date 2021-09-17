b98db64a47db4b2e89ca3b6921e83e4ab3e4e948
Xingxin HE <xingxin.he@mail.polimi.it>
2021-09-18 04:37:19 +0800

    Assignment 00

---
+++ Lecture/DiscreteDifferentialGeometry/code/00_simplicial-complex-operators/src/simplicial-complex-operators.cpp
@@ -0,0 +1,233 @@
+// Implement member functions for SimplicialComplexOperators class.
+#include "simplicial-complex-operators.h"
+
+using namespace geometrycentral;
+using namespace geometrycentral::surface;
+
+/*
+ * Assign a unique index to each vertex, edge, and face of a mesh.
+ * All elements are 0-indexed.
+ *
+ * Input: None. Access geometry via the member variable <geometry>, and pointer to the mesh via <mesh>.
+ * Returns: None.
+ */
+void SimplicialComplexOperators::assignElementIndices() {
+
+    // Needed to access geometry->vertexIndices, etc. as cached quantities.
+    // Not needed if you're just using v->getIndex(), etc.
+    geometry->requireVertexIndices();
+    geometry->requireEdgeIndices();
+    geometry->requireFaceIndices();
+
+    // You can set the index field of a vertex via geometry->vertexIndices[v], where v is a Vertex object (or an
+    // integer). Similarly you can do edges and faces via geometry->edgeIndices, geometry->faceIndices, like so:
+    size_t idx = 0;
+    for (Vertex v : mesh->vertices()) {
+        idx = geometry->vertexIndices[v];
+    }
+
+    for (Edge e : mesh->edges()) {
+        idx = geometry->edgeIndices[e];
+    }
+
+    for (Face f : mesh->faces()) {
+        idx = geometry->faceIndices[f];
+    }
+
+    // You can more easily get the indices of mesh elements using the function getIndex(), albeit less efficiently and
+    // technically less safe (although you don't need to worry about it), like so:
+    //
+    //      v.getIndex()
+    //
+    // where v can be a Vertex, Edge, Face, Halfedge, etc. For example:
+
+    for (Vertex v : mesh->vertices()) {
+        idx = v.getIndex(); // == geometry->vertexIndices[v])
+    }
+
+    // Geometry Central already sets the indices for us, though, so this function is just here for demonstration.
+    // You don't have to do anything :)
+}
+
+/*
+ * Construct the unsigned vertex-edge adjacency matrix A0.
+ *
+ * Input:
+ * Returns: The sparse vertex-edge adjacency matrix which gets stored in the global variable A0.
+ */
+SparseMatrix<size_t> SimplicialComplexOperators::buildVertexEdgeAdjacencyMatrix() const {
+
+    // TODO
+    // Note: You can build an Eigen sparse matrix from triplets, then return it as a Geometry Central SparseMatrix.
+    // See <https://eigen.tuxfamily.org/dox/group__TutorialSparse.html> for documentation.
+
+    return identityMatrix<size_t>(1); // placeholder
+}
+
+/*
+ * Construct the unsigned face-edge adjacency matrix A1.
+ *
+ * Input:
+ * Returns: The sparse face-edge adjacency matrix which gets stored in the global variable A1.
+ */
+SparseMatrix<size_t> SimplicialComplexOperators::buildFaceEdgeAdjacencyMatrix() const {
+
+    // TODO
+    return identityMatrix<size_t>(1); // placeholder
+}
+
+/*
+ * Construct a vector encoding the vertices in the selected subset of simplices.
+ *
+ * Input: Selected subset of simplices.
+ * Returns: Vector of length |V|, where |V| = # of vertices in the mesh.
+ */
+Vector<size_t> SimplicialComplexOperators::buildVertexVector(const MeshSubset& subset) const {
+
+    // TODO
+    return Vector<size_t>::Zero(1);
+}
+
+/*
+ * Construct a vector encoding the edges in the selected subset of simplices.
+ *
+ * Input: Selected subset of simplices.
+ * Returns: Vector of length |E|, where |E| = # of edges in mesh.
+ */
+Vector<size_t> SimplicialComplexOperators::buildEdgeVector(const MeshSubset& subset) const {
+
+    // TODO
+    return Vector<size_t>::Zero(1);
+}
+
+/*
+ * Construct a vector encoding the faces in the selected subset of simplices.
+ *
+ * Input: Selected subset of simplices.
+ * Returns: Vector of length |F|, where |F| = # of faces in mesh.
+ */
+Vector<size_t> SimplicialComplexOperators::buildFaceVector(const MeshSubset& subset) const {
+
+    // TODO
+    return Vector<size_t>::Zero(1);
+}
+
+/*
+ * Compute the simplicial star St(S) of the selected subset of simplices.
+ *
+ * Input: A MeshSubset object containing the indices of the currently active vertices, edges, and faces, respectively.
+ * Returns: The star of the given subset.
+ */
+MeshSubset SimplicialComplexOperators::star(const MeshSubset& subset) const {
+    MeshSubset star = subset.deepCopy();
+
+    for (size_t iVertex : star.vertices)
+    {
+        for (Edge e : mesh->vertex(iVertex).adjacentEdges())
+        {
+            star.addEdge(e.getIndex());
+        }
+        for (Face f : mesh->vertex(iVertex).adjacentFaces())
+        {
+            star.addFace(f.getIndex());
+        }
+    }
+    
+    return star;
+}
+
+
+/*
+ * Compute the closure Cl(S) of the selected subset of simplices.
+ *
+ * Input: A MeshSubset object containing the indices of the currently active vertices, edges, and faces, respectively.
+ * Returns: The closure of the given subset.
+ */
+MeshSubset SimplicialComplexOperators::closure(const MeshSubset& subset) const {
+    
+    //Make a deepcopy of the input simplices
+    MeshSubset closure = subset.deepCopy();
+
+    // For closure, we only need to 
+    // (1) add the adjacent edges from faces
+    // (2) add the adjacent vertices from faces
+    // (3) add the adjacent vertices from edges
+    for (size_t iFace : subset.faces)
+    {
+        for (Edge e : mesh->face(iFace).adjacentEdges())
+        {
+            closure.addEdge(e.getIndex());
+        }
+        for (Vertex v : mesh->face(iFace).adjacentVertices())
+        {
+            closure.addVertex(v.getIndex());
+        }
+    }
+
+    for (size_t iEdge : subset.edges)
+    {
+        for (Vertex v : mesh->edge(iEdge).adjacentVertices())
+        {
+            closure.addVertex(v.getIndex());
+        }
+    }
+
+    return closure;
+}
+
+/*
+ * Compute the link Lk(S) of the selected subset of simplices.
+ *
+ * Input: A MeshSubset object containing the indices of the currently active vertices, edges, and faces, respectively.
+ * Returns: The link of the given subset.
+ */
+MeshSubset SimplicialComplexOperators::link(const MeshSubset& subset) const {
+
+    MeshSubset closure = SimplicialComplexOperators::closure(SimplicialComplexOperators::star(subset));
+    MeshSubset star = SimplicialComplexOperators::star(SimplicialComplexOperators::closure(subset));
+
+    closure.deleteSubset(star);
+
+    return closure;
+}
+
+/*
+ * Return true if the selected subset is a simplicial complex, false otherwise.
+ *
+ * Input: A MeshSubset object containing the indices of the currently active vertices, edges, and faces, respectively.
+ * Returns: True if given subset is a simplicial complex, false otherwise.
+ */
+bool SimplicialComplexOperators::isComplex(const MeshSubset& subset) const {
+
+    MeshSubset closure = SimplicialComplexOperators::closure(subset);
+    
+    return subset.equals(closure);
+}
+
+/*
+ * Check if the given subset S is a pure simplicial complex. If so, return the degree of the complex. Otherwise, return
+ * -1.
+ *
+ * Input: A MeshSubset object containing the indices of the currently active vertices, edges, and faces, respectively.
+ * Returns: int representing the degree of the given complex (-1 if not pure)
+ */
+int SimplicialComplexOperators::isPureComplex(const MeshSubset& subset) const {
+
+    if (!SimplicialComplexOperators::isComplex(subset))
+    {
+        return -1;
+    }
+
+}
+
+/*
+ * Compute the set of simplices contained in the boundary bd(S) of the selected subset S of simplices.
+ *
+ * Input: A MeshSubset object containing the indices of the currently active vertices, edges, and faces, respectively.
+ * Returns: The boundary of the given subset.
+ */
+MeshSubset SimplicialComplexOperators::boundary(const MeshSubset& subset) const {
+
+    // TODO
+    return subset; // placeholder
+}
